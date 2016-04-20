adfa36d92e2d0e0112ae8c081ba344a784d7839f
Diego Borges <drborges.cic@gmail.com>
2016-04-20 09:57:37 -0300

    Implement one-way databinding

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    return <div>Hello there!</div>
+
+  constructor() {
+    super()
+    this.state = { input: '' }
   }
+
+  update(e) {
+    this.setState({ input: e.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.props.greeting}</div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.input}</h1>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  greeting: React.PropTypes.string
+}
+
+App.defaultProps = {
+  greeting: 'Hello World!'
 }
 
 export default App
