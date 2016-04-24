1968d966e0ccc302116a72ed2dfdd66b818c2329
Diego Borges <drborges.cic@gmail.com>
2016-04-23 21:28:36 -0300

    Implement firebase GET and POST abstractions

--- App.js
+++ App.js
@@ -1,53 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Firebase from 'firebase';
+import { autobind } from 'core-decorators';
 
-@FirebaseModel('/users')
-class App extends React.Component {
-  render() {
-    let rows = this.state.users.map((user, id) => {
-      return <UserRow key={id} data={user} />
-    })
+let firebase = {
+  GET: (path) => {
+    return (Target) => {
+      return class extends Target {
 
-    return (
-      <ul>
-        {rows}
-      </ul>
-    )
-  }
-}
+        constructor() {
+          super()
+          this.key = path.slice(1)
+          this.state = this.state || {}
+          this.state[this.key] = {}
+          this.fref = new Firebase(`https://userxp.firebaseio.com${path}`)
+        }
 
-const UserRow = (props) => {
-  return (
-    <li>
-      <span>User: {props.data.name} ({props.data.level})</span>
-    </li>
-  )
-}
-
-function FirebaseModel(path) {
-  return function Decorator(Target) {
-    return class Decorated extends Target {
-
-      constructor() {
-        super()
-        this.key = path.slice(1)
-        this.state = {}
-        this.state[this.key] = []
+        componentWillMount() {
+          this.fref.on("child_added", function(dataSnapshot) {
+            this.state[this.key][dataSnapshot.key()] = dataSnapshot.val()
+            this.setState(this.state);
+          }.bind(this));
+        }
       }
+    }
+  },
 
-      componentWillMount() {
-        this.fref = new Firebase(`https://userxp.firebaseio.com${path}`)
+  POST: (path) => {
+    return (Target) => {
+      return class extends Target {
+        constructor() {
+          super()
+          this.key = path.slice(1)
+          this.state = this.state || {}
+          this.fref = new Firebase(`https://userxp.firebaseio.com${path}`)
+        }
 
-        this.fref.on("child_added", function(dataSnapshot) {
-          this.state[this.key][dataSnapshot.key()] = dataSnapshot.val()
-          this.setState(this.state);
-        }.bind(this));
+        @autobind
+        post() {
+          console.log(this.state)
+          let ref = this.fref.push()
+          ref.set(this.state)
+          console.log(ref.toString())
+        }
       }
     }
   }
 }
 
+@firebase.GET('/users')
+class App extends React.Component {
+  render() {
+    let rows = Object.keys(this.state.users).map((id) => {
+      return <UserRow key={id} data={this.state.users[id]} />
+    })
 
+    return (
+      <div>
+        <NewUser />
+        <ul>
+          {rows}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+@firebase.POST('/users')
+class NewUser extends React.Component {
+
+  @autobind
+  onNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  @autobind
+  onLevelChange(e) {
+    this.setState({ level: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text"
+               value={this.state.name}
+               onChange={this.onNameChange} />
+        <input type ="text"
+               value={this.state.level}
+               onChange={this.onLevelChange} />
+             <button onClick={this.post}>Create</button>
+      </div>
+    )
+  }
+}
+
+const UserRow = (props) =>
+  <li>
+    <span>User: {props.data.name} ({props.data.level})</span>
+  </li>
 
 export default App
